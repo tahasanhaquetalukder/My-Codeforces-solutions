55a39bfd1efcff398e61cda316b825eccedcfe34
tahasanhaquetalukder <tahasan5800p@gamil.com>
2026-05-11 23:42:49 +0600

    Problem no:1703A

---
+++ A_YES_or_YES.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t!=0)
+    {
+        string s;
+    cin>>s;
+    int count=0;
+    if(s[0]=='Y' || s[0]=='y')
+    {
+        count++;
+    }
+     if(s[1]=='E' || s[1]=='e')
+     {
+        count++;
+     }
+     if(s[2]=='S' || s[2]=='s')
+     {
+        count++;
+     }
+     if(count==3)
+     {
+        cout<<"YES"<<endl;
+     }
+     else
+     {
+        cout<<"NO"<<endl;
+     }
+        t--;
+    }
+     
+    return 0;
+}
