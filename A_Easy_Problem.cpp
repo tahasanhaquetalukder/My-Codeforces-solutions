6dbc23c235fd7bcbe467488d33f8ae75fac04c96
tahasanhaquetalukder <tahasan5800p@gamil.com>
2026-05-11 22:15:44 +0600

    Problem: 2044A

---
+++ A_Easy_Problem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n!=0)
+    {
+        int a;
+        cin>>a;
+        cout<<a-1<<endl;
+        n--;
+    }
+}
