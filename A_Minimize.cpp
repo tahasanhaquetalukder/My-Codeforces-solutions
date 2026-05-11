93c5809f875b20c1076095eef8ab7828f0b351e6
tahasanhaquetalukder <tahasan5800p@gamil.com>
2026-05-12 00:42:14 +0600

    solution

---
+++ A_Minimize.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n!=0)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<b-a<<endl;
+        n--;       
+    }
+    return 0;
+}
